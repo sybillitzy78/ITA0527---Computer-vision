cbbebb5326082a79b1b4950aee62b6af44f22530
sybillitzy78 <sybillitzy2006@gmail.com>
2026-02-28 11:12:48 +0530

    Delete exp1.py Gray Scale.py

--- exp1.py Gray Scale.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread("C:/Users/sybil/Downloads/hotel .jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-cv2.imshow("Gray Image", gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
